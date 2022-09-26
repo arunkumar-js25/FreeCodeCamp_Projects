08a6dae6b0830df63918c6065557593d18f955c6
arunkumar-js25 <arunkumar.js25@gmail.com>
2022-09-26 22:10:16 +0530

    Day8 update

---
+++ JavaScript Algorithms and Data Structures/Projects/Palindrome Checker.js
@@ -0,0 +1,16 @@
+function palindrome(str) {
+  const replaced = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+  let len = replaced.length;
+  let i= 0, j=len-1;
+  while(j >=i ){
+    if(replaced[i] != replaced[j]){
+      return false;
+    }
+    i++;
+    j--;
+  }
+
+  return true;
+}
+
+palindrome("eye");
--- JavaScript Algorithms and Data Structures/readMe.txt
+++
@@ -1,1 +0,0 @@
-Basic javascript Problems: 113
